508270eb1b5f7e59ed3c11e1aa95448beb40203f
ibrahim Osama <zx.hema72@gmail.com>
2024-03-15 21:20:11 +0200

    Add carousel images to Assets class
    Add carousel images carousel_1.png, carousel_2.png, and carousel_3.png to the
    Assets class for easy access in the application.

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,9 +1,21 @@
 class Assets {
   Assets._();
-
+  
   /// Assets for imagesBookHistory
   /// assets/images/book_history.svg
   static const String imagesBookHistory = "assets/images/book_history.svg";
+
+  /// Assets for imagesCarousel1
+  /// assets/images/carousel_1.png
+  static const String imagesCarousel1 = "assets/images/carousel_1.png";
+
+  /// Assets for imagesCarousel2
+  /// assets/images/carousel_2.png
+  static const String imagesCarousel2 = "assets/images/carousel_2.png";
+
+  /// Assets for imagesCarousel3
+  /// assets/images/carousel_3.png
+  static const String imagesCarousel3 = "assets/images/carousel_3.png";
 
   /// Assets for imagesHome
   /// assets/images/home.svg
@@ -45,3 +57,4 @@ class Assets {
   /// assets/images/welcome.svg
   static const String imagesWelcome = "assets/images/welcome.svg";
 }
+
