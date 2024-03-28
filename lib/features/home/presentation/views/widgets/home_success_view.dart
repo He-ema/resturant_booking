351170abd1b8339f31253d0ba7f8cba223a7eb6e
ibrahim Osama <zx.hema72@gmail.com>
2024-03-29 00:18:41 +0200

    Add trending section to HomeSuccessView widget
    Introduce a new section in the HomeSuccessView widget displaying trending
    content with appropriate styling and alignment.

--- lib/features/home/presentation/views/widgets/home_success_view.dart
+++ lib/features/home/presentation/views/widgets/home_success_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jobizz/core/utils/app_styles.dart';
 import 'package:jobizz/features/home/presentation/views/widgets/carousel_and_dots.dart';
 import 'package:jobizz/features/home/presentation/views/widgets/custom_header.dart';
 
@@ -24,6 +25,31 @@ class HomeSuccessView extends StatelessWidget {
         ),
         const SliverToBoxAdapter(
           child: CarouselAndDots(),
+        ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Trending', style: AppStyles.styleSemiBold16),
+                    Text('Best of this week', style: AppStyles.styleMedium12),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'See all >',
+                      style: AppStyles.styleMedium12,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
         SliverFillRemaining(
           hasScrollBody: false,
